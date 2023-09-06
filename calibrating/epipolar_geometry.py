559dae4980d3a93f1ef086fde2df797bebbad973
Lei <ylxx@live.com>
2023-09-06 18:59:18 +0800

    depth scale spread

--- calibrating/epipolar_geometry.py
+++ calibrating/epipolar_geometry.py
@@ -9,15 +9,15 @@ with boxx.inpkg():
     from .utils import inv, R_t_to_T, apply_T_to_point_cloud
 
 
-def compute_essential_matrix(xyz1, xyz2):
-    assert xyz1.shape == xyz2.shape, "The shapes of input points must be the same."
-    n = xyz1.shape[0]
+def compute_essential_matrix(xyzs1, xyzs2):
+    assert xyzs1.shape == xyzs2.shape, "The shapes of input points must be the same."
+    n = xyzs1.shape[0]
     assert n >= 8, "At least 8 point pairs are required."
     # 构建矩阵A
     A = np.zeros((n, 9))
     for i in range(n):
-        x1, y1, z1 = xyz1[i]
-        x2, y2, z2 = xyz2[i]
+        x1, y1, z1 = xyzs1[i]
+        x2, y2, z2 = xyzs2[i]
         A[i] = [
             x1 * x2,
             x2 * y1,
@@ -94,21 +94,34 @@ def matched_xyz_normals_to_zs(xyz_normals1, xyz_normals2, T_1to2):
 
 
 class EssentialMatrixStereo(Stereo):
-    def __init__(self, uv1, uv2, K1, K2=None, baseline=1, xy1=None, xy2=None):
+    def __init__(
+        self,
+        uvs1,
+        uvs2,
+        K1,
+        K2=None,
+        baseline=1,
+        xy1=None,
+        xy2=None,
+        name1="cam1",
+        name2="cam2",
+    ):
         if K2 is None:
             K2 = K1
-        xyz_normals1 = uvs_to_xyz_noramls(uv1, K1)
-        xyz_normals2 = uvs_to_xyz_noramls(uv2, K2)
-        self.E = compute_essential_matrix(xyz_normals1, xyz_normals2)
-        T_1to2s = decompose_essential_matrix(self.E)
+        assert name1 != name2
+        xyz_normals1 = uvs_to_xyz_noramls(uvs1, K1)
+        xyz_normals2 = uvs_to_xyz_noramls(uvs2, K2)
+        E = compute_essential_matrix(xyz_normals1, xyz_normals2)
+        T_1to2s = decompose_essential_matrix(E)
 
         for T in T_1to2s:
             # z2_z1 = calibrating.apply_T_to_point_cloud(T, xyz_normals1)/xyz_normals2
             # print(z2_z1)
 
             T[:3, 3] *= baseline / np.linalg.norm(T[:3, 3])
-            re = matched_xyz_normals_to_zs(xyz_normals1, xyz_normals2, T)
-            z1, z2 = re["zs1"].mean(), re["zs2"].mean()
+            # TODO: Dose this need slice to recduce compuate?
+            zs = matched_xyz_normals_to_zs(xyz_normals1, xyz_normals2, T)
+            z1, z2 = zs["zs1"].mean(), zs["zs2"].mean()
             if z1 > 0 and z2 > 0:
                 break
         if boxx.mg():
@@ -118,42 +131,66 @@ class EssentialMatrixStereo(Stereo):
         super().__init__()
         self.R, self.t = T[:3, :3], T[:3, 3]
         self.load(
-            dict(R=self.R, t=self.t, cam1=dict(xy=xy1, K=K1), cam2=dict(xy=xy2, K=K2))
+            dict(
+                R=self.R,
+                t=self.t,
+                cam1=dict(xy=xy1, K=K1, name=name1),
+                cam2=dict(xy=xy2, K=K2, name=name2),
+            )
         )
+        self.epipolar = zs
+        self.epipolar["E"] = E
+        self.epipolar["uvs1"] = uvs1
+        self.epipolar["uvs2"] = uvs2
+        self.epipolar["z1"] = z1
+        self.epipolar["z2"] = z2
 
     @classmethod
-    def from_stereo(cls, uv1, uv2, stereo, baseline=None):
-        return cls(
-            uv1,
-            uv2,
+    def from_stereo(cls, uvs1, uvs2, stereo, baseline=None):
+        self = cls(
+            uvs1,
+            uvs2,
             K1=stereo.cam1.K,
             K2=stereo.cam2.K,
             xy1=stereo.cam1.xy,
             xy2=stereo.cam2.xy,
             baseline=baseline or stereo.baseline,
         )
+        dic = stereo.dump(return_dict=1)
+        dic["R"], dic["t"] = self.R, self.t
+        self.load(dic)
+        return self
+
+
+def filter_overlap_uvs(uvs1, uvs2):
+    _, inverse, count_ = np.unique(
+        np.int32(uvs1.round()), axis=0, return_counts=True, return_inverse=True
+    )
+    count1 = count_[inverse]
+
+    _, inverse, count_, = np.unique(
+        np.int32(uvs2.round()), axis=0, return_counts=True, return_inverse=True
+    )
+    count2 = count_[inverse]
+    mask = (count1 <= 1) & (count2 <= 1)
+    return uvs1[mask], uvs2[mask]
 
 
 if __name__ == "__main__":
-    import cv2
     from boxx import *
     from calibrating import get_test_cams, T_to_deg_distance, perturb_T
 
-    cam1, cam2, camd = get_test_cams("aruco").values()
+    cam1, cam2, cam3 = get_test_cams("aruco").values()
     key = list(cam1)[0]
-    img1, img2 = (
-        boxx.imread(cam1[key]["path"]),
-        boxx.imread(cam2[key]["path"]),
-    )
-    # img1_undistort = cv2.undistort(img1, cam1.K, cam1.D)
-    # img2_undistort = cv2.undistort(img2, cam2.K, cam2.D)
+    img1 = boxx.imread(cam1[key]["path"])
+    img2 = boxx.imread(cam2[key]["path"])
     stereo = Stereo(cam1, cam2)
-    # {"K1":stereo.K.tolist(),"K2":stereo.K.tolist(), "uv1uv2": np.random.randint(1,1024,(20,4)).tolist()}
-    uv1, uv2, obj = stereo.get_conjoint_points()
-    uv1_distort = np.concatenate(uv1, 0)
-    uv2_distort = np.concatenate(uv2, 0)
-    uv1 = cam1.undistort_points(uv1_distort)
-    uv2 = cam1.undistort_points(uv2_distort)
+    # {"K1":stereo.K.tolist(),"K2":stereo.K.tolist(), "uvs1uvs2": np.random.randint(1,1024,(20,4)).tolist()}
+    uvs1, uvs2, obj_points = stereo.get_conjoint_points()
+    uvs1_distort = np.concatenate(uvs1, 0)
+    uvs2_distort = np.concatenate(uvs2, 0)
+    uvs1 = cam1.undistort_points(uvs1_distort)
+    uvs2 = cam1.undistort_points(uvs2_distort)
     T_gt = R_t_to_T(stereo.R, stereo.t)
 
     random_test_case = 0  # + 1
@@ -167,17 +204,86 @@ if __name__ == "__main__":
             uvzs = apply_T_to_point_cloud(T, obj_points) @ K.T
             return uvzs[:, :2] / uvzs[:, 2:]
 
-        uv1 = np.concatenate([imaging(cam1.K, T) for T in T_boards])
-        uv2 = np.concatenate([imaging(cam2.K, T_gt @ T) for T in T_boards])
+        uvs1 = np.concatenate([imaging(cam1.K, T) for T in T_boards])
+        uvs2 = np.concatenate([imaging(cam2.K, T_gt @ T) for T in T_boards])
 
-    stereo2 = EssentialMatrixStereo.from_stereo(
-        uv1, uv2, stereo, baseline=np.linalg.norm(T_gt[:3, 3])
+    uvs1, uvs2 = filter_overlap_uvs(uvs1, uvs2)
+    estereo = EssentialMatrixStereo.from_stereo(
+        uvs1, uvs2, stereo, baseline=np.linalg.norm(T_gt[:3, 3])
     )
-    stereo2 = Stereo.load(dict(T=stereo2.T, cam1=cam1, cam2=cam2))
-    T_re = stereo2.T
+    T_re = estereo.T
     print("T_gt:  ", T_to_deg_distance(T_gt))
     print("T_re:  ", T_to_deg_distance(T_re))
     print("T_diff:", T_to_deg_distance(T_gt @ inv(T_re)))
 
     if not random_test_case:
-        stereo2.shows((*stereo2.rectify(img1, img2)))
+        estereo.shows((*estereo.rectify(img1, img2)))
+
+    """
+    单位尺度: 主视图的平均深度为 1 个单位深度
+    尺度传导: 一个视野, 分别和另外两个视野求了匹配的 uvs, 需要求出能被三个视野共同看见的 uvs, 用来传导尺度: 
+        - 真实情况: 先 round, 做粗糙匹配, 互相匹配, 取 uv 距离最低的 20% 和 max(100) 来
+        - stereo 关键点仿真数据: 由于单个像素会存在对应两张图片的关键点的情况, 所以需要去除这种同一 uv 坐标的多个关键点
+    """
+    #%%
+    img3 = boxx.imread(cam3[key]["path"])
+    stereo23 = Stereo(cam2, cam3)
+    uvs3, uvs4, obj_points = stereo23.get_conjoint_points()
+    uvs3 = cam1.undistort_points(np.concatenate(uvs3, 0))
+    uvs4 = np.concatenate(uvs4, 0)
+    uvs3, uvs4 = filter_overlap_uvs(uvs3, uvs4)
+    estereo23 = EssentialMatrixStereo.from_stereo(uvs3, uvs4, stereo23, baseline=1)
+    # stereo23.shows((*stereo23.rectify(img2, img3)))
+    # estereo23.shows((*estereo23.rectify(img2, img3)))
+
+    stereo1, stereo2 = estereo, estereo23
+    names1 = stereo1.cam1.name, stereo1.cam2.name
+    names2 = stereo2.cam1.name, stereo2.cam2.name
+    assert len(set(names1)) == 2, names1
+    assert len(set(names2)) == 2, names2
+    assert (
+        len(set(names1 + names2)) != 2
+    ), f"names1={names1}, names2={names2} has exactly the same names!"
+    assert (
+        len(set(names1 + names2)) != 4
+    ), f"names1={names1}, names2={names2} has total different names!"
+    for name in names1:
+        if name in names2:
+            break
+    s1_suffix = str(names1.index(name) + 1)
+    s2_suffix = str(names2.index(name) + 1)
+    uvs1 = stereo1.epipolar["uvs" + s1_suffix]
+    uvs2 = stereo2.epipolar["uvs" + s2_suffix]
+
+    from scipy.spatial import KDTree
+
+    kd_tree = KDTree(uvs1)
+    distance, nearst_idx = kd_tree.query(uvs2)
+    MAX_DISTANCE = 1
+    mask = distance < MAX_DISTANCE
+
+    k = int((uvs1.size + uvs2.size) / 2 * 0.1)
+
+    if mask.sum() > 10 and mask.sum() > k:
+        arg1 = np.argpartition(distance, k)[:k]
+        mask_ = mask * False
+        mask_[arg1] = mask[arg1]
+        mask = mask_
+    MIN_MATCHED_PIXELS = 10
+    if mask.sum() < MIN_MATCHED_PIXELS:
+        assert mask.sum() >= MIN_MATCHED_PIXELS, mask.sum()
+
+    uv_match_idx2 = np.arange(0, mask.size)[mask]
+    uv_match_idx1 = nearst_idx[mask]
+
+    z_matched1 = stereo1.epipolar["zs" + s1_suffix][uv_match_idx1]
+    z_matched2 = stereo2.epipolar["zs" + s2_suffix][uv_match_idx2]
+    stereo2.t *= z_matched1.mean() / z_matched2.mean()
+
+    #%%
+    stereo13 = Stereo(cam1, cam3)
+    stereo13_re = Stereo.load(dict(T=stereo2.T @ stereo1.T, cam1=cam1, cam2=cam3))
+
+    print(stereo13, stereo13_re)
+    stereo13.shows((*stereo13.rectify(img1, img3)))
+    stereo13_re.shows((*stereo13_re.rectify(img1, img3)))
